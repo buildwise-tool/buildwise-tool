5fc11cdc359df5d3942657ae91ab5da079f1a71c
Forum Suthar <forum.suthar@gmail.com>
2024-11-24 16:55:18 -0800

    Skipping Vite Certificate creation for CI/CD

--- src/buildwise.client/vite.config.ts
+++ src/buildwise.client/vite.config.ts
@@ -16,17 +16,23 @@ const certificateName = "buildwise.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
+if( env.CI != undefined)
+{
+    console.log('CI is set to' + env.CI);
+}
+if( env.CI == undefined || env.CI != 'true' ) {
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+        if (0 !== child_process.spawnSync('dotnet', [
+            'dev-certs',
+            'https',
+            '--export-path',
+            certFilePath,
+            '--format',
+            'Pem',
+            '--no-password',
+        ], { stdio: 'inherit', }).status) {
         throw new Error("Could not create certificate.");
+        }
     }
 }
 
